d711a78a3a0036e3936a84653af364a008aaa5f6
Satit Rianpit <rianpit@gmail.com>
2016-09-17 17:47:08 +0700

    success emr detail

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ router.get('/list', function(req, res, next) {
 
   jwt.verify(token)
     .then((decoded) => {
-        let sql = `SELECT HOSPCODE, PID, NAME, LNAME, SEX FROM tmp_dengue LIMIT 10`;
+        let sql = `SELECT HOSPCODE, PID, NAME, LNAME, SEX, DATE_SERV FROM tmp_dengue LIMIT 10`;
         db.raw(sql, [])
           .then(rows => {
             res.send({ ok: true, rows: rows[0] });
@@ -30,7 +30,7 @@ router.post('/search', function(req, res, next) {
 
   jwt.verify(token)
     .then((decoded) => {
-      let sql = `SELECT HOSPCODE, PID, NAME, LNAME, SEX
+      let sql = `SELECT HOSPCODE, PID, NAME, LNAME, SEX, DATE_SERV
         FROM tmp_dengue
         WHERE NAME LIKE ?
         LIMIT 10`;
@@ -56,7 +56,7 @@ router.post('/date-filter', function(req, res, next) {
 
   jwt.verify(token)
     .then((decoded) => {
-      let sql = `SELECT HOSPCODE, PID, NAME, LNAME, SEX
+      let sql = `SELECT HOSPCODE, PID, NAME, LNAME, SEX, DATE_SERV
         FROM tmp_dengue
         WHERE DATE_SERV=?
         LIMIT 10`;
@@ -88,6 +88,36 @@ router.post('/emr', function(req, res, next) {
         where concat(d.HOSPCODE, d.PID)=?
         `;
       db.raw(sql, [hpid])
+        .then(rows => {
+          res.send({ ok: true, rows: rows[0] });
+        })
+        .catch(err => {
+          res.send({ ok: false, msg: err.message })
+        });
+    }, err => {
+      console.log(err);
+      res.send({ok: false, msg: 'Invalid token!'})
+    });
+
+});
+
+router.post('/emr-detail', function(req, res, next) {
+  let db = req.db;
+  let token = req.body.token;
+  let hpid = req.body.hpid;
+  let dateServ = req.body.dateServ;
+
+  jwt.verify(token)
+    .then((decoded) => {
+      let sql = `select d.DATE_SERV as date_serv, d.DIAGCODE as diagcode, t.diagtname, h.hoscode, h.hosname,
+      d.VILLAGENAME as village, d.TAMBONNAME as tambon, d.AMPURNAME as ampur
+      from tmp_dengue as d
+      left join cicd10tm as t on t.diagcode=d.DIAGCODE
+      left join chospital as h on h.hoscode=d.HOSPCODE
+      where concat(d.HOSPCODE, d.PID)=? and d.DATE_SERV=?
+      limit 1
+        `;
+      db.raw(sql, [hpid, dateServ])
         .then(rows => {
           res.send({ ok: true, rows: rows[0] });
         })
